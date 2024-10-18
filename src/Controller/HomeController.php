76abee6b15cd19ac8d2dd1ce70501c81a3a022fc
Yoan Martins <102329074+Yooo31@users.noreply.github.com>
2024-10-18 15:56:37 +0200

    Feat/swagger: Add swagger doc for API (#2)
    * feat:Add new swagger conf
    
    * feat:Add new swagger conf
    
    * feat:Add swagger on login and logout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,11 +7,16 @@ use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 #[Route('/', name: 'home.')]
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'index', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Show Home page'
+    )]
     public function index(TaskRepository $taskRepository, PriorityRepository $priorityRepository): Response
     {
         $tasks = $taskRepository->findAll() ?: "No tasks found";
