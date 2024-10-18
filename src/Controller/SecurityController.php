270725271ba865113ca3177a53d916d055b11757
Yoan <mrts.yoan@gmail.com>
2024-10-18 15:52:53 +0200

    feat:Add swagger on login and logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,25 +6,59 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use OpenApi\Attributes as OA;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'login', methods: ['GET', 'POST'])]
+    #[OA\RequestBody(
+        required: true,
+        content: new OA\JsonContent(
+            type: 'object',
+            required: ['mail', 'password'],
+            properties: [
+                new OA\Property(property: 'mail', type: 'string', example: 'john@doe.com'),
+                new OA\Property(property: 'password', type: 'string', example: 'mypasswordencrypted')
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Login success',
+        content: new OA\JsonContent(
+            type: 'object',
+            properties: [
+                new OA\Property(property: 'message', type: 'string', example: 'Login success')
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Invalid credentials',
+        content: new OA\JsonContent(
+            type: 'object',
+            properties: [
+                new OA\Property(property: 'error', type: 'string', example: 'Invalid credentials')
+            ]
+        )
+    )]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home.index');
+        }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'logout', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Logout success',
+    )]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
