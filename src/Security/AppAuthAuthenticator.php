1ba58afd17c22f4e72aa1a1b7272176e76aecc84
Yoan <mrts.yoan@gmail.com>
2024-11-06 08:51:39 +0100

    feat:tasks-Correction of login name route

--- src/Security/AppAuthAuthenticator.php
+++ src/Security/AppAuthAuthenticator.php
@@ -20,7 +20,7 @@ class AppAuthAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
-    public const LOGIN_ROUTE = 'app_login';
+    public const LOGIN_ROUTE = 'login';
 
     public function __construct(private UrlGeneratorInterface $urlGenerator)
     {
